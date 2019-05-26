ac613585ab3613d94a66a8a6550e3a7141404462
thundo811 <gvnnino811@gmail.com>
2019-05-26 17:45:18 +0700

    Updates

---
+++ precache-manifest.4080e7cfda29a4ea0347fd868ff96f1e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa58879a453bb878ace89916fbb99c8",
+    "url": "/gameplaytest/index.html"
+  },
+  {
+    "revision": "8d948d0e3a7329042149",
+    "url": "/gameplaytest/static/js/2.950e1f53.chunk.js"
+  },
+  {
+    "revision": "14620f1d1d6048f1f097",
+    "url": "/gameplaytest/static/js/main.ba70e80a.chunk.js"
+  },
+  {
+    "revision": "ca21782af87e240691f0",
+    "url": "/gameplaytest/static/js/runtime~main.6dcbbeb3.js"
+  }
+]);
